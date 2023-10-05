30d2f8e4955e2f0ab6c8972bbf8152abe2fbcd6c
Sivaram.Alluri <105623683+sivaramalluri@users.noreply.github.com>
2023-10-05 20:39:42 +0530

    File Added

---
+++ HibernateComponentMapping/src/main/java/ProductDetails.java
@@ -0,0 +1,81 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import com.ecommerce.EProduct;
+import com.ecommerce.HibernateUtil;
+import com.ecommerce.ProductParts;
+
+
+/**
+* Servlet implementation class ProductDetails
+*/
+@WebServlet("/ProductDetails")
+public class ProductDetails extends HttpServlet {
+        private static final long serialVersionUID = 1L;
+       
+        
+        
+    /**
+* @see HttpServlet#HttpServlet()
+*/
+    public ProductDetails() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+        /**
+         * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+         */
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+                // TODO Auto-generated method stub
+                 try {
+                        SessionFactory factory = HibernateUtil.getSessionFactory();
+                        
+                        Session session = factory.openSession();
+                        
+                        
+                         
+                        List<EProduct> list = session.createQuery("from EProduct").list();
+                        
+                         PrintWriter out = response.getWriter();
+                         out.println("<html><body>");
+                         
+                         out.println("<b>Component Mapping</b><br>");
+                         for(EProduct p: list) {
+                                 out.println("ID: " + String.valueOf(p.getID()) + ", Name: " + p.getName() +
+                                                 ", Price: " + String.valueOf(p.getPrice()) + ", Date Added: " + p.getDateAdded().toString());
+                                 ProductParts parts = (ProductParts) p.getParts();
+                                 out.println("Parts =" + parts.getCpu() + ", " + parts.getHdd() + ", " + parts.getRam());
+                                 out.println("<hr>");
+                         }
+                         
+                                session.close();
+
+                     out.println("</body></html>");
+                     
+                     
+                 } catch (Exception ex) {
+                         throw ex;
+                 }
+                    
+        }
+
+        /**
+         * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+         */
+        protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+                // TODO Auto-generated method stub
+                doGet(request, response);
+        }
+
+}
